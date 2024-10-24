52ee94fa5b6837fafa1106ff20e4ac4744eda8c6
Karan Shrivastava <work.karan2001@gmail.com>
2024-10-24 14:16:45 -0400

    Updates

--- src/pages/QuizletPage.js
+++
@@ -1,9 +0,0 @@
-// QuizletPage.js
-import React from 'react';
-
-const QuizletPage = () => {
-  window.location.href = 'https://quizlet.com'; // Redirect to Quizlet
-  return null;
-};
-
-export default QuizletPage;
